9fa7ed2ffee334546a3deffdbd080435371f9a7a
anjana732 <anjanasingh1257@gmail.com>
2024-04-07 21:22:41 +0530

    added experience section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
-
+import experience from './data/experience.json'
 function Experience() {
   return (
     <>
     <div className="container ex">
       <h1>EXPERIENCE</h1>
+      {
+        experience.map((data) => {
+          return(
+            <>
+            <div key={data.id} className='ex-item text-centre'>
+              <div className="left">
+                <img src={`/assets/${data.imageSrc}`} alt=""/>
+              </div>
+              <div className="right">
+                <h2>{data.role}</h2>
+                <h4>{data.startDate} {data.endDate} {data.location}</h4>
+                <h5>{data.experiences[0]}</h5>
+                <h5>{data.experiences[1]}</h5>
+              </div>
+            </div>
+            </>
+          )
+        })
+      }
     </div>
     </>
   )
